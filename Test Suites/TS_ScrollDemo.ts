2f5eb89124a832835883ebabb97ae100dbaf36b1
Meghana_Kandregula <Meghana_Kandregula@epam.com>
2022-11-29 08:37:53 +0530

    Scroll Demo

---
+++ Test Suites/TS_ScrollDemo.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_ScrollDemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fe633dd4-942c-4fe7-9821-5513024fac0a</testSuiteGuid>
+   <testCaseLink>
+      <guid>63e8b0f9-5d05-42e5-9ac8-b3ae275a5049</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_SrollDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
